dea4b7d9953aded8abd4348c1db75a467aaa8028
sancrack235 <sancrack267@gmail.com>
2025-09-18 12:05:29 -0300

    EMPEZE BACKEND WACHO

---
+++ Frontend/Registro/main.js
@@ -0,0 +1,11 @@
+import { subscribeGETEvent, subscribePOSTEvent, realTimeEvent, startServer } from "soquetic";n
+
+const usuario = document.getElementById('Usuario');
+const contraseña = document.getElementById('Contraseña');
+const form = document.getElementById('form');
+
+
+
+form.addEventListener('submit', (e) => {
+    
+});
