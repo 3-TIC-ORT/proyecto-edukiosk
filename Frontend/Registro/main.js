9eef4faa5e75c61e3a15b53a9accc0cb269a87d0
Rocco276 <roccoaltieri@gmail.com>
2025-09-19 22:34:20 -0300

    Mejore

---
+++ Frontend/Registro/main.js
@@ -0,0 +1,32 @@
+const usuario = document.getElementById("Usuario");
+const contraseña = document.getElementById("Contraseña");
+const mail = document.getElementById("Email");
+const form = document.getElementById("form");
+
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const nombreUsuario = usuario.value;
+  const contraseñaUsuario = contraseña.value;
+  const mailUsuario = mail.value;
+
+
+  fetch("http://localhost:3000/api/registro", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      usuario: nombreUsuario,
+      contraseña: contraseñaUsuario,
+      mail: mailUsuario,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      alert(data.message);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      alert("Error al registrar el usuario");
+    });
+});
