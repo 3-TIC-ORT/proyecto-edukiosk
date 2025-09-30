5de98472cdd53288bbc0d43c4496a3590bf41171
sancrack235 <sancrack267@gmail.com>
2025-09-30 08:08:53 -0300

    Estableci parametros del front de registro

--- Frontend/Registro/main.js
+++ Frontend/Registro/main.js
@@ -1,9 +1,42 @@
-import soquetic from "soquetic";
-
 const usuario = document.getElementById("Usuario");
 const contraseña = document.getElementById("Contraseña");
-const mail = document.getElementById("Email");
+const mail = document.getElementById("Correo");
 const form = document.getElementById("form");
+const caracteresProhibidos = [
+  " ",
+  "!",
+  "#",
+  "$",
+  "%",
+  "&",
+  "/",
+  "(",
+  ")",
+  "=",
+  "?",
+  "¡",
+  "¿",
+  "*",
+  "+",
+  "-",
+  "_",
+  "{",
+  "}",
+  "[",
+  "]",
+  "^",
+  "~",
+  "`",
+  ";",
+  ",",
+  ".",
+  "<",
+  ">",
+  ":",
+  '"',
+  "'",
+  "\\",
+];
 
 connect2Server(3000);
 
@@ -12,9 +45,31 @@ form.addEventListener("submit", (e) => {
   const nombreUsuario = usuario.value;
   const contraseñaUsuario = contraseña.value;
   const mailUsuario = mail.value;
-  
-  postEvent("info", { user: nombreUsuario, password: contraseñaUsuario, email: mailUsuario }, (data) => {
-    
-  })
 
+  if (
+    nombreUsuario.includes(caracteresProhibidos) ||
+    nombreUsuario.length >= 15
+  ) {
+    alert(
+      "El nombre de usuario no puede contener espacios ni caracteres especiales o exceder los 15 caracteres."
+    );
+    nombreUsuario.value = "";
+    contraseñaUsuario.value = "";
+    mailUsuario.value = "";
+  }
+  if (!mailUsuario.includes("ort.edu.ar" && "@")) {
+    alert("El correo debe ser institucional (terminar en @est.ort.edu.ar/@ort.edu.ar)");
+  } else {
+    postEvent(
+      "info",
+      { user: nombreUsuario, password: contraseñaUsuario, email: mailUsuario },
+      (data) => {
+        if (!success) {
+          alert("Algo salió mal, o el mail/usuario ya está registrado");
+        } else {
+          alert("Registro exitoso");
+        }
+      }
+    );
+  }
 });
